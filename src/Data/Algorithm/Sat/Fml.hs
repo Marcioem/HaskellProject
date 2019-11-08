9d0dd03ac5db214c94486204f648722dc0d776b1
Marcioem <43565693+Marcioem@users.noreply.github.com>
2019-11-08 17:49:39 +0100

    Add toCNF function

--- src/Data/Algorithm/Sat/Fml.hs
+++ src/Data/Algorithm/Sat/Fml.hs
@@ -16,6 +16,19 @@ data Fml a = Or (Fml a) (Fml a)
 
 
 toCNF :: Fml a -> Fml a
+toCNF (Final a ) = a
+toCNF (Imply a b ) = toCNF( Or (Not a) (b) )
+toCNF (Equiv a b ) = toCNF( Or (And (a) (b)) (And (Not a) (Not b)))
+toCNF (XOr a b ) = toCNF( Or (And (a)(Not b)) (And (Not a) (b)))
+toCNF (Not a) = f a
+  where
+  f (Final b ) = Not b
+  f (Not b) = toCNF (a)
+  f (Or b c ) = toCNF( And(Not b) (Not c))
+  f (And b c ) = toCNF( Or ( b c))
+toCNF ( And a b ) = And (toCNF a) (toCNF b)
+toCNF ( Or a b ) = 
+
 vars :: Fml a -> [Var.Var a]
 prettyPrinter :: Fml a -> String
 atLeast :: (Eq t, Num t, Ord a) => t ->[Fml.Fml a]-> Fml.Fml a
